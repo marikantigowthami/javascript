78b4df4a4688b246df7a07b97985c2e7e44ff990
marikantigowthami <marikantigowthami90@gmail.com>
2025-09-22 14:28:14 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+// commit used to understand the code by other developer
+//data types
+//1.primitive 2. non-premitive
+//primitive - Number,String,Boolen,Null,Undefined
+//Non-Primitive - Array,Object
+// typeof()-know the type of data
+//this,window- commands to display the functions of js
+
+a = 7897;
+console.log(typeof a);
+name = "Gowthami";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
+
+//non premitive
+array = [56, "Gowthami", true, null];
+console.log(typeof array);
+data = {
+  name: "Gowthami",
+  age: "20",
+  dept: "AIML",
+};
+console.log(typeof data);
